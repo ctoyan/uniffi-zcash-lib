26517519a4439bf1075132815ce9077b06d28d24
Stoyan Kirov <stoian.kiroff@gmail.com>
2023-10-11 17:17:40 +0300

    start extracting functions into files and use outputs

---
+++ scripts/get-libs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# be stricter
+set -eo pipefail
+
+# Get librustzcash libraries used in uniffi-zcash-lib
+get_libs() {
+	local output
+	output=$(
+		cargo metadata --format-version=1 --no-deps --manifest-path=./librustzcash/Cargo.toml |
+			jq -r '.packages[] | .name' |
+			xargs -I {} sh -c "cargo metadata --format-version=1 --no-deps --manifest-path=./uniffi-zcash-lib/lib/Cargo.toml | jq -r '.packages[] | .dependencies[] | .name' | grep '{}' | sort -u"
+	)
+
+	echo "$output"
+}
+
+main() {
+	libs=$(get_libs)
+	echo "$libs"
+}
+
+main "$@"
